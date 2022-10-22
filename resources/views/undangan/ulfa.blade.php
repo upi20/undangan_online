033ea3e27a85217fe5acdf2d344115f37c4ce536
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2022-10-22 19:36:24 +0700

    update fix bug halaman utama

--- resources/views/undangan/ulfa.blade.php
+++ resources/views/undangan/ulfa.blade.php
@@ -283,7 +283,6 @@
 
         </section>
 
-
         <section
             class="elementor-section elementor-top-section elementor-element elementor-element-74641fe5 elementor-section-boxed elementor-section-height-default elementor-section-height-default wdp-sticky-section-no"
             data-id="74641fe5" data-element_type="section" id="doa"
@@ -325,7 +324,6 @@
                 </div>
             </div>
         </section>
-
 
         <section
             class="elementor-section elementor-top-section elementor-element elementor-element-22f13031 elementor-section-boxed elementor-section-height-default elementor-section-height-default wdp-sticky-section-no"
@@ -648,10 +646,6 @@
             </div>
         </section>
 
-
-
-
-
         <section
             class="elementor-section elementor-top-section elementor-element elementor-element-4dfc27a0 elementor-section-boxed elementor-section-height-default elementor-section-height-default wdp-sticky-section-no"
             data-id="4dfc27a0" data-element_type="section" id="timeline"
@@ -668,6 +662,7 @@
                 </div>
             </div>
         </section>
+
         <section
             class="elementor-section elementor-top-section elementor-element elementor-element-4c12da71 elementor-section-boxed elementor-section-height-default elementor-section-height-default wdp-sticky-section-no"
             data-id="4c12da71" data-element_type="section" id="mempelai"
@@ -1171,107 +1166,6 @@
                                         </script>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        <section
-            class="elementor-section elementor-top-section elementor-element elementor-element-765b5253 elementor-section-boxed elementor-section-height-default elementor-section-height-default wdp-sticky-section-no"
-            data-id="765b5253" data-element_type="section"
-            data-settings="{&quot;shape_divider_top&quot;:&quot;mountains&quot;,&quot;background_background&quot;:&quot;classic&quot;}">
-            <div class="elementor-shape elementor-shape-top" data-negative="false">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none">
-                    <path class="elementor-shape-fill" opacity="0.33"
-                        d="M473,67.3c-203.9,88.3-263.1-34-320.3,0C66,119.1,0,59.7,0,59.7V0h1000v59.7 c0,0-62.1,26.1-94.9,29.3c-32.8,3.3-62.8-12.3-75.8-22.1C806,49.6,745.3,8.7,694.9,4.7S492.4,59,473,67.3z" />
-                    <path class="elementor-shape-fill" opacity="0.66"
-                        d="M734,67.3c-45.5,0-77.2-23.2-129.1-39.1c-28.6-8.7-150.3-10.1-254,39.1 s-91.7-34.4-149.2,0C115.7,118.3,0,39.8,0,39.8V0h1000v36.5c0,0-28.2-18.5-92.1-18.5C810.2,18.1,775.7,67.3,734,67.3z" />
-                    <path class="elementor-shape-fill"
-                        d="M766.1,28.9c-200-57.5-266,65.5-395.1,19.5C242,1.8,242,5.4,184.8,20.6C128,35.8,132.3,44.9,89.9,52.5C28.6,63.7,0,0,0,0 h1000c0,0-9.9,40.9-83.6,48.1S829.6,47,766.1,28.9z" />
-                </svg>
-            </div>
-            <div class="elementor-container elementor-column-gap-default">
-                <div class="elementor-row">
-                    <div class="elementor-column elementor-col-100 elementor-top-column elementor-element elementor-element-32c43096 wdp-sticky-section-no"
-                        data-id="32c43096" data-element_type="column">
-                        <div class="elementor-column-wrap elementor-element-populated">
-                            <div class="elementor-widget-wrap">
-                                <div class="elementor-element elementor-element-5895e9f4 wdp-sticky-section-no elementor-widget elementor-widget-spacer"
-                                    data-id="5895e9f4" data-element_type="widget" data-widget_type="spacer.default">
-                                    <div class="elementor-widget-container">
-                                        <div class="elementor-spacer">
-                                            <div class="elementor-spacer-inner">
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="elementor-element elementor-element-2648b24a wdp-sticky-section-no elementor-widget elementor-widget-heading"
-                                    data-id="2648b24a" data-element_type="widget" data-widget_type="heading.default">
-                                    <div class="elementor-widget-container">
-                                        <h2 class="elementor-heading-title elementor-size-default">
-                                            Made with ♥ by</h2>
-                                    </div>
-                                </div>
-                                <section
-                                    class="elementor-section elementor-inner-section elementor-element elementor-element-40e5804c elementor-section-boxed elementor-section-height-default elementor-section-height-default wdp-sticky-section-no"
-                                    data-id="40e5804c" data-element_type="section">
-                                    <div class="elementor-container elementor-column-gap-no">
-                                        <div class="elementor-row">
-                                            <div class="elementor-column elementor-col-50 elementor-inner-column elementor-element elementor-element-1653b656 wdp-sticky-section-no"
-                                                data-id="1653b656" data-element_type="column">
-                                                <div class="elementor-column-wrap elementor-element-populated">
-                                                    <div class="elementor-widget-wrap">
-                                                        <div class="elementor-element elementor-element-47c9693 wdp-sticky-section-no elementor-widget elementor-widget-image"
-                                                            data-id="47c9693" data-element_type="widget"
-                                                            data-widget_type="image.default">
-                                                            <div class="elementor-widget-container">
-                                                                <div class="elementor-image">
-                                                                    <a class="elementor-image-link"
-                                                                        href="https://www.instagram.com/agrainvitation/">
-                                                                        <img width="300" height="93"
-                                                                            src="{{ asset('template/wp-content/uploads/2021/04/agra-logo-1-300x93.png') }}"
-                                                                            class="attachment-medium size-medium"
-                                                                            alt="" loading="lazy" />
-                                                                    </a>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="elementor-column elementor-col-50 elementor-inner-column elementor-element elementor-element-19b12898 wdp-sticky-section-no"
-                                                data-id="19b12898" data-element_type="column">
-                                                <div class="elementor-column-wrap elementor-element-populated">
-                                                    <div class="elementor-widget-wrap">
-                                                        <div class="elementor-element elementor-element-2c84dce4 elementor-align-left elementor-mobile-align-left wdp-sticky-section-no elementor-widget elementor-widget-button_lp"
-                                                            data-id="2c84dce4" data-element_type="widget"
-                                                            data-widget_type="button_lp.default">
-                                                            <div class="elementor-widget-container">
-                                                                <div class="elementor-button-wrapper">
-                                                                    <a href="https://www.instagram.com/agrainvitation/"
-                                                                        class="elementor-button-link elementor-button elementor-size-md"
-                                                                        role="button">
-                                                                        <span class="elementor-button-content-wrapper">
-                                                                            <span
-                                                                                class="elementor-button-icon elementor-align-icon-left">
-                                                                                <i aria-hidden="true"
-                                                                                    class="fab fa-instagram"></i>
-                                                                            </span>
-                                                                            <span
-                                                                                class="elementor-button-text">agrainvitation</span>
-                                                                        </span>
-                                                                    </a>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </section>
                             </div>
                         </div>
                     </div>
@@ -2908,7 +2802,6 @@
                 </div>
             </div>
         </section>
-
 
         <div class="elementor-element elementor-element-74b5572a elementor-view-default wdp-sticky-section-no elementor-widget elementor-widget-weddingpress-audio"
             data-id="74b5572a" data-element_type="widget" data-widget_type="weddingpress-audio.default"
